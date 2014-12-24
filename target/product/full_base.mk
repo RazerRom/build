b0a9406837acf38b0131858542296c508be601d6
Shaun Eaton <shaunsibu@gmail.com>
2014-12-24 11:57:53 -0700

    Revert "Build: slim up sounds and update (2/2)"
    This reverts commit 361a78137a7fbb4b2a5c68f3d434d89cc7685059.

--- target/product/full_base.mk
+++ target/product/full_base.mk
@@ -30,8 +30,6 @@ PRODUCT_PACKAGES := \
 # Additional settings used in all AOSP builds
 PRODUCT_PROPERTY_OVERRIDES := \
     ro.com.android.dateformat=MM-dd-yyyy
-    ro.config.ringtone=Triton.ogg \
-    ro.config.notification_sound=Io.ogg
 
 # Put en_US first in the list, so make it default.
 PRODUCT_LOCALES := en_US
@@ -40,7 +38,7 @@ PRODUCT_LOCALES := en_US
 PRODUCT_AAPT_CONFIG := normal hdpi xhdpi xxhdpi
 
 # Get some sounds
-$(call inherit-product-if-exists, frameworks/base/data/sounds/AllAudioBliss.mk)
+$(call inherit-product-if-exists, frameworks/base/data/sounds/AllAudio.mk)
 
 # Get the TTS language packs
 $(call inherit-product-if-exists, external/svox/pico/lang/all_pico_languages.mk)
