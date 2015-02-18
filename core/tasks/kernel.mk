80dd4c4aa78af623b9f07d517ab84517e21860ed
Shaun Eaton <shaunsibu@gmail.com>
2015-02-18 14:11:06 -0700

    Add missing items from CP and also fix dialog in dumpvar.
    Change-Id: I9284cff7ca08335e3a6ef68a221070cb6b891924

--- core/tasks/kernel.mk
+++ core/tasks/kernel.mk
@@ -166,7 +166,16 @@ ifneq ($(USE_CCACHE),)
     ccache := $(strip $(wildcard $(ccache)))
 endif
 
-KERNEL_CROSS_COMPILE := CROSS_COMPILE="$(ccache) $(KERNEL_TOOLCHAIN_PATH)"
+ifneq ($(TARGET_TC_KERNEL),)
+  ifeq ($(HOST_OS),darwin)
+    KERNEL_CROSS_COMPILE := CROSS_COMPILE="$(ANDROID_BUILD_TOP)/prebuilts/gcc/darwin-x86/arm/arm-eabi-$(TARGET_TC_KERNEL)/bin/arm-eabi-"
+  else
+    KERNEL_CROSS_COMPILE := CROSS_COMPILE="$(ANDROID_BUILD_TOP)/prebuilts/gcc/linux-x86/arm/arm-eabi-$(TARGET_TC_KERNEL)/bin/arm-eabi-"
+  endif
+else
+  KERNEL_CROSS_COMPILE := CROSS_COMPILE="$(ccache) $(KERNEL_TOOLCHAIN_PATH)"
+endif
+
 ccache =
 
 define mv-modules
