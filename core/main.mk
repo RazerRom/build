46f102c0b0e5906504acfb6b3afb103ffd0b1654
tobitege <tobiasteschner@googlemail.com>
2015-04-13 21:32:29 +0200

    Add intermediate and ota files to make dirty
    Change-Id: Ib3d448d3e0deabfd9fe055eb49bf8859d24a3d13

--- core/main.mk
+++ core/main.mk
@@ -1117,11 +1117,12 @@ magic:
 .PHONY: dirty
 dirty:
 	@rm -rf $(OUT_DIR)/target/product/*/system/build.prop
-	@rm -rf $(OUT_DIR)/target/product/*/ota.xml
+	@rm -rf $(OUT_DIR)/target/product/*/ota*.xml
 	@rm -rf $(OUT_DIR)/target/product/*/*.zip
 	@rm -rf $(OUT_DIR)/target/product/*/*.md5sum
 	@rm -rf $(OUT_DIR)/target/product/*/*.txt
-	@echo -e ${CL_GRN}"build.prop, changelog and zip files erased"${CL_RST}	
+	@rm -rf $(OUT_DIR)/target/product/*/obj/PACKAGING/target_files_intermediates/*.*
+	@echo -e ${CL_GRN}"build.prop, ota*.xml, md5sum, .txt and .zip files erased"${CL_RST}
 
 # Clears out all apks
 .PHONY: appclean
