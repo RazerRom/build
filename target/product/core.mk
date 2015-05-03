7dc06227ab91d85bce9e07cd5b4e680e02e0c71a
gamerman123x <19superguy98@gmail.com>
2015-05-03 02:38:53 -0400

    dont build apps
    Signed-off-by: gamerman123x <19superguy98@gmail.com>

--- target/product/core.mk
+++ target/product/core.mk
@@ -22,9 +22,7 @@
 
 PRODUCT_PACKAGES += \
     BasicDreams \
-    Browser \
     Calculator \
-    Calendar \
     CalendarProvider \
     CaptivePortalLogin \
     CertInstaller \
@@ -32,8 +30,6 @@ PRODUCT_PACKAGES += \
     DeskClock \
     DocumentsUI \
     DownloadProviderUi \
-    Email \
-    Exchange2 \
     ExternalStorageProvider \
     FusedLocation \
     InputDevices \
