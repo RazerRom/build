9fa97bde9ca969d69b3cd012f8fc1800a9e23d33
gamerman123x <19superguy98@gmail.com>
2015-06-25 21:16:51 -0400

    test

--- tools/roomservice.py
+++ tools/roomservice.py
@@ -58,7 +58,7 @@ def check_repo_exists(git_data):
 def search_github_for_device(device):
     git_device = '+'.join(re.findall('[a-z]+|[\d]+',  device))
     git_search_url = "https://api.github.com/search/repositories" \
-                     "?q=%40{}+android_device+{}+fork:true".format(android_team, git_device)
+                     "?q=%40{}+device+{}+fork:true".format(android_team, git_device)
     git_req = urllib.request.Request(git_search_url)
     try:
         response = urllib.request.urlopen(git_req)
@@ -75,9 +75,9 @@ def get_device_url(git_data):
     device_url = ""
     for item in git_data['items']:
         temp_url = item.get('html_url')
-        if "{}/android_device".format(android_team) in temp_url:
+        if "{}/device".format(android_team) in temp_url:
             try:
-                temp_url = temp_url[temp_url.index("android_device"):]
+                temp_url = temp_url[temp_url.index("device"):]
             except ValueError:
                 pass
             else:
@@ -92,7 +92,7 @@ def get_device_url(git_data):
 
 
 def parse_device_directory(device_url,device):
-    to_strip = "android_device"
+    to_strip = "device"
     repo_name = device_url[device_url.index(to_strip) + len(to_strip):]
     repo_name = repo_name[:repo_name.index(device)]
     repo_dir = repo_name.replace("_", "/")
@@ -203,7 +203,7 @@ def write_to_manifest(manifest):
 def parse_device_from_manifest(device):
     for project in iterate_manifests(True):
         name = project.get('name')
-        if name.startswith("android_device_") and name.endswith(device):
+        if name.startswith("device_") and name.endswith(device):
             return project.get('path')
     return None
 
