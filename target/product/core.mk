0a2bd6af4c1d3b0df6fecf4d748e9f8b51d1e2ec
gamerman123x <19superguy98@gmail.com>
2015-06-29 14:15:28 -0400

    Remove apps
    Signed-off-by: gamerman123x <19superguy98@gmail.com>

--- target/product/core.mk
+++ target/product/core.mk
@@ -21,7 +21,6 @@
 # core_tiny.mk in addition to core_minimal.mk.
 
 PRODUCT_PACKAGES += \
-    BasicDreams \
     Calculator \
     CalendarProvider \
     CaptivePortalLogin \
@@ -35,14 +34,11 @@ PRODUCT_PACKAGES += \
     InputDevices \
     KeyChain \
     Keyguard \
-    Launcher2 \
     ManagedProvisioning \
-    PicoTts \
     PacProcessor \
     libpac \
     PrintSpooler \
     ProxyHandler \
-    QuickSearchBox \
     Settings \
     SharedStorageBackup \
     Telecom \
